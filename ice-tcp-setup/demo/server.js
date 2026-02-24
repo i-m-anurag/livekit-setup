d2ddb7557b826134f56839dc699ab5c8ea26dc34
Anurag <anuragkumar849@gmail.com>
2026-02-24 13:57:33 +0530

    CORS fix

--- ice-tcp-setup/demo/server.js
+++ ice-tcp-setup/demo/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 const { AccessToken } = require("livekit-server-sdk");
 
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Must match keys in livekit.yaml
